8c04719bb8ad3b05641e3ec2c429ed834b224c35
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2023-11-25 23:00:27 -0500

    [add] - Counter design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(child: Text('Hello world!')),
-      ),
+      home: CounterScreen(),
     );
   }
 }
